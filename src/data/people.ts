73a779886ae0346fcc3d8b36fd059e55d3b83b27
Piotr <92919637+my-alaska@users.noreply.github.com>
2026-08-15 13:21:23 +0200

    Add Piotr Ludynia (#2)

--- src/data/people.ts
+++ src/data/people.ts
@@ -54,7 +54,28 @@ export const PEOPLE_GROUPS: PeopleGroup[] = [
   {
     id: 'phd',
     title: 'PhD students',
-    members: [],
+    members: [
+      {
+        name: 'Piotr Ludynia',
+        role: 'Researcher, Faculty of Computer Science, AGH',
+        photo: 'img/people/Piotr_Ludynia.png',
+        bio: 'One of the original creators of scikit-fingerprints. Mainly interested in ML algorithm opitmizations, especially in chemoinformatics.',
+        links: [
+          {
+            label: 'Scholar',
+            href: 'https://scholar.google.com/citations?user=By_zWVoAAAAJ&hl=en',
+          },
+          { 
+            label: 'GitHub', 
+            href: 'https://github.com/my-alaska' 
+          },
+          {
+            label: 'LinkedIn',
+            href: 'https://www.linkedin.com/in/piotr-ludynia/',
+          },
+        ],
+      }
+    ],
   },
   {
     id: 'students',
